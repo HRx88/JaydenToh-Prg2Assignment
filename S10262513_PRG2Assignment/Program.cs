0ce4b37ae30b3b0a7306a9b5bbc5e2a560b8b3b9
HRx88 <S10262513@connect.np.edu.sg>
2024-01-22 17:37:05 +0800

    doing q5

--- S10262513_PRG2Assignment/Program.cs
+++ S10262513_PRG2Assignment/Program.cs
@@ -3,6 +3,7 @@ using S10262513_PRG2Assignment;
 using System.Collections.Generic;
 using static System.Formats.Asn1.AsnWriter;
 using System.IO;
+using System.Collections;
 //==========================================================
 // Student Number : S10262513
 // Student Name : Tan Hong Rong
@@ -28,13 +29,15 @@ void Listallcustomers()
             customersList.Add(new Customer(name, id, dob));
 
         }
+
+        foreach (Customer customer in customersList)
+        {
+            Console.WriteLine(customer);
+        }
     }
 }
 Listallcustomers();
-foreach (Customer customer in customersList)
-{
-    Console.WriteLine(customer);
-}
+
 //2) List all current orders
 //display the information of all current orders in both the gold members and regular queue
 Queue<Customer>orders = new Queue<Customer>();
@@ -65,12 +68,17 @@ void DisplaycOrders()
 
 
 
-
-
-
-
-
-
         }
     }
+}
+//5)Display order details of a customer
+// list the customers
+// prompt user to select a customer and retrieve the selected customer
+// retrieve all the order objects of the customer, past and current
+// for each order, display all the details of the order including datetime received, datetime
+//fulfilled (if applicable) and all ice cream details associated with the order
+void Display_order_details_of_a_customer()
+{
+    Listallcustomers();
+    Console.WriteLine("select a customer: ");
 }
